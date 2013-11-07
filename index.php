76061f32b1dfc3cbd3fd0093509051c20008a11f
Jürgen 'tante' Geuter <tante@tante.cc>
2013-11-07 21:01:57 +0100

    Die ersten Anfragen

--- index.php
+++ index.php
@@ -10,7 +10,20 @@
 <?php
 if(isset($_REQUEST['Suche'])){
     print("Suchbegriff: ".$_REQUEST['Suche']);
+    $url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&lr=lang_de&cr=countryDE&".$_REQUEST['Suche'];
+    print("Query: ".$URL);
 
+    //Anfrage senden 
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_REFERER, "Weltnetzsuchantrieb.de");
+    $inhalte = curl_exec($ch);
+    curl_close($ch);
+
+    $ergebnisse = json_decode($inhalte);
+
+    print_r($ergebnisse);
 }
 ?>
 </body>
